de4ed269d3308b3561dee43fde766fd361f6dc60
Tahiana Daluz <tahiana.daluz@gmail.com>
2022-09-28 21:58:08 -0300

    punto2

--- js/index.js
+++ js/index.js
@@ -1,51 +1,63 @@
 let btnSearch = document.getElementById("btnBuscar");
 let inputSearch = document.getElementById("inputBuscar");
 let URL_DATA = "https://japceibal.github.io/japflix_api/movies-data.json"
-let result = {};
-
+let moviesArray = [];
 document.addEventListener("DOMContentLoaded", e => {
     fetchData();
 })
-
 btnSearch.addEventListener("click", e => {
     if (inputSearch.value.length > 0) {
         searchMovie();
     }
 })
-
 const fetchData = async () => {
-    try {
-        const response = await fetch(URL_DATA);
-        if (response.ok) {
-            result.data = await response.json();
-            result.status = "ok";
-        } else {
-            throw Error(response.statusText);
-        }
+    const response = await fetch(URL_DATA);
+    if (response.ok) {
+        moviesArray = await response.json();
+    } else {
+        throw Error(response.statusText);
     }
-    catch (error) {
-        result.status = 'error';
-        result.data = error;
-    }
-    return result;  
+    console.log(moviesArray);
+}
+function searchMovie() {
+    let arraySearch = [];
+    arraySearch = moviesArray.filter(({ title, overview, tagline, genres }) =>
+        title.toLowerCase().includes((inputSearch.value).toLowerCase()) || 
+        overview.toLowerCase().includes((inputSearch.value).toLowerCase()) ||
+        tagline.toLowerCase().includes((inputSearch.value).toLowerCase()) ||
+        genres.some(({name}) => name.toLocaleLowerCase().includes(inputSearch.value)))
+    console.log(arraySearch)
+    showMovie(arraySearch);
 }
 
-/* function searchMovie(){
-    let array = [];
-        for(let i = 0; i < result.data.length; i++){
-        let category = result.data[i];
-        let valueInclude = (category.title).includes(inputSearch.value, category)
-        if (valueInclude) {
-        console.log(category.title); 
-        } 
-}}*/ 
-
-function searchMovie(){
-    let array = [];
-        for(let i = 0; i < result.data.length; i++){
-        let category = result.data[i];
-        let valueInclude = (category.title).includes(inputSearch.value, category)
-        if (valueInclude) {
-        console.log(category.title); 
-        } 
+function showMovie(showArray){
+let htmlContentToAppend = "";
+    for(let i = 0; i < showArray.length; i++){
+    let movies = showArray[i];
+    console.log(movies.title);
+    htmlContentToAppend += `
+    <div class="list-group-item list-group-item-action " id="listComment">
+    <div class="d-flex w-100 justify-content-between">
+    <div class="mb-1">
+        <h5>${movies.title}</h5>
+        <p>${movies.tagline}</p>
+    </div>
+    </div>
+    </div>
+    `
+    document.getElementById("lista").innerHTML = htmlContentToAppend; 
 }}
+
+
+for (let i = 0; i < prodcutCommentArray.length; i++) {
+    let productData = prodcutCommentArray[i];
+    htmlContentToAppend += `
+    <p><b>${productData.user}</b> ${productData.description}</p>
+    <p>${productData.dateTime} `
+    for (let x = 0; x < productData.score ; x++) {
+    htmlContentToAppend += `<span class="fa fa-star score"></span>`
+    }       
+    for (let x = 0; x < (5 - productData.score); x++) {
+    htmlContentToAppend += `<span class="fa fa-star "></span>`
+    } 
+  }
