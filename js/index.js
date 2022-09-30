e011bad528c3664901e62c0daa172a7d22b29fd5
Tahiana Daluz <tahiana.daluz@gmail.com>
2022-09-30 20:12:16 -0300

    add

--- js/index.js
+++ js/index.js
@@ -22,39 +22,49 @@ const fetchData = async () => {
 function searchMovie() {
     let arraySearch = [];
     arraySearch = moviesArray.filter(({ title, overview, tagline, genres }) =>
-        title.toLowerCase().includes((inputSearch.value).toLowerCase()) || 
+        title.toLowerCase().includes((inputSearch.value).toLowerCase()) ||
         overview.toLowerCase().includes((inputSearch.value).toLowerCase()) ||
         tagline.toLowerCase().includes((inputSearch.value).toLowerCase()) ||
-        genres.some(({name}) => name.toLocaleLowerCase().includes(inputSearch.value)))
-    console.log(arraySearch)
+        genres.some(({ name }) => name.toLocaleLowerCase().includes(inputSearch.value)))
     showMovie(arraySearch);
+
 }
 
 function showMovie(showArray) {
-  let htmlContentToAppend = "";
-  htmlContentToAppend += `
-  <div class="list-group-item list-group-item-action " id="listComment">
-     <div class="d-flex w-100 justify-content-between">`
-      for (let i = 0; i < showArray.length; i++) {
-       let movies = showArray[i];
-        console.log(movies.title);
+    let htmlContentToAppend = "";
+    let aa = "";
+    for (let i = 0; i < showArray.length; i++) {
+        let movies = showArray[i];
         htmlContentToAppend += `
-         <div class="mb-1">
-          <h5>${movies.title}</h5>
-          <p>${movies.tagline}</p>
-          <p>`
-            for (let x = 0; x < (parseInt(movies.vote_average/2)) ; x++) {
-             htmlContentToAppend += `<span class="fa fa-star checked"></span>`
-            }       
-            for (let x = 0; x < (5 - (parseInt(movies.vote_average/2))); x++) {
-             htmlContentToAppend += `<span class="fa fa-star"></span>`
-            } 
-        `</p>
-       </div>`;
-  }
- ` </div>
-  </div>`;
+        <div class="list-group-item list-group-item-action cursor-active">
+        <div class="row">
+        <div class="col">
+        <div class="d-flex w-100 justify-content-between">
+        <div class="mb-1">
+            <h5>${movies.title} </h5>
+            <p>${movies.tagline}</p>
+            <div id="stars" "stars(${movies.vote_average})"></div>
+        </div>
+    </div>
+    </div>
+    </div>
+    </div>  
+        `
+        document.getElementById("lista").innerHTML = htmlContentToAppend;
 
-  document.getElementById("lista").innerHTML = htmlContentToAppend;
+    }
+
 }
 
+function stars(aa) {
+    let htmlContentToAppend = "";
+    console.log(aa.vote_average)
+        varm = parseInt(aa / 2)
+        for (let x = 0; x < varm; x++) {
+            htmlContentToAppend += `<span class="fa fa-star checked"></span>`
+        }
+        for (let x = 0; x < (5 - varm); x++) {
+            htmlContentToAppend += `<span class="fa fa-star"></span>`
+        }
+        
+    }
