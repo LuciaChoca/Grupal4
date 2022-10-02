d0ec5e8f7dce9ba43137680950782b4a23c6a87e
LuciaChoca <luchoca7@gmail.com>
2022-10-02 11:30:59 -0300

    cambioooos

--- js/index.js
+++ js/index.js
@@ -2,14 +2,19 @@ let btnSearch = document.getElementById("btnBuscar");
 let inputSearch = document.getElementById("inputBuscar");
 let URL_DATA = "https://japceibal.github.io/japflix_api/movies-data.json"
 let moviesArray = [];
+
+let canva = document.getElementById("boton");
+
 document.addEventListener("DOMContentLoaded", e => {
     fetchData();
 })
+
 btnSearch.addEventListener("click", e => {
     if (inputSearch.value.length > 0) {
         searchMovie();
     }
-})
+
+});
 const fetchData = async () => {
     const response = await fetch(URL_DATA);
     if (response.ok) {
@@ -27,6 +32,7 @@ function searchMovie() {
         tagline.toLowerCase().includes((inputSearch.value).toLowerCase()) ||
         genres.some(({ name }) => name.toLocaleLowerCase().includes(inputSearch.value)))
     showMovie(arraySearch);
+    showMovies(arraySearch);
     Stars2(arraySearch)
 }
 
@@ -36,7 +42,7 @@ function showMovie(showArray) {
     for (let i = 0; i < showArray.length; i++) {
         let movies = showArray[i];
         htmlContentToAppend += `
-        <div class="list-group-item list-group-item-action cursor-active">
+        <div class="list-group-item list-group-item-action cursor-active" onclick="">
         <div class="row">
         <div class="col">
         <div class="d-flex w-100 justify-content-between">
@@ -49,13 +55,45 @@ function showMovie(showArray) {
     </div>
     </div>
     </div>  
+
         `
         document.getElementById("lista").innerHTML = htmlContentToAppend;
-     //  Stars(movies.vote_average, movies.id)
+        
+        
+       
+        //  Stars(movies.vote_average, movies.id)
     }
- 
+
 }
 
+function showMovies(arraySearch) {
+    let htmlContentToAppend5 = "";
+   arraySearch.forEach(element => {
+    htmlContentToAppend5 = `
+            <h5>${element.title} </h5>
+           
+            <p>${element.tagline}</p>
+           
+            
+        `
+        document.getElementById("title").innerHTML = htmlContentToAppend5;
+   });
+    /*for (let i = 0; i < showArray.length; i++) {
+        let movies = showArray[i];
+        htmlContentToAppend = `
+            <h5>${movies.title} </h5>
+            <p>${movies.tagline}</p>
+            <div id="${movies.id}"></div>
+            
+        `
+        document.getElementById("title").innerHTML = htmlContentToAppend;
+        
+        
+       
+        //  Stars(movies.vote_average, movies.id)
+    }*/
+    
+}
 /*function Stars(vote_average, id) {
     let htmlContentToAppend1 = "";
 
@@ -74,23 +112,52 @@ function showMovie(showArray) {
         document.getElementById(id).innerHTML = htmlContentToAppend1;
     }*/
 
-    function Stars2(showArray) {
-        
-        for (let i = 0; i < showArray.length; i++) {
-            let htmlContentToAppend1 = "";
-            let movie = showArray[i];
-     
-            //let aa = showArray[i];
-            console.log(movie.vote_average)
-    
-    
-            varm = parseInt(movie.vote_average / 2)
-            for (let x = 0; x < varm; x++) {
-                htmlContentToAppend1 += `<span class="fa fa-star checked"></span>`
-            }
-            for (let x = 0; x < (5 - varm); x++) {
-                htmlContentToAppend1 += `<span class="fa fa-star"></span>`
-            }
-            document.getElementById(movie.id).innerHTML = htmlContentToAppend1;
+function Stars2(showArray) {
+
+    for (let i = 0; i < showArray.length; i++) {
+        let htmlContentToAppend1 = "";
+        let movie = showArray[i];
+
+        //let aa = showArray[i];
+        console.log(movie.vote_average)
+
+
+        varm = parseInt(movie.vote_average / 2)
+        for (let x = 0; x < varm; x++) {
+            htmlContentToAppend1 += `<span class="fa fa-star checked"></span>`
         }
+        for (let x = 0; x < (5 - varm); x++) {
+            htmlContentToAppend1 += `<span class="fa fa-star"></span>`
+        }
+        document.getElementById(movie.id).innerHTML = htmlContentToAppend1;
+    
     }
+}
+       
+
+
+// tendria que ser una funcion show, que cuando clickeamos el div aparezca el canva
+// y un add even listener que cuando toque el div me meta la funcion 
+//ONCLICK EN HTML QUE LLAME LA FUNCION
+
+/*
+canva.addEventListener("click", canvaBox, false);
+
+function canvaBox(event) {
+  let warn = `${moviesArray.title} `;
+  let warn2 = ` ${moviesArray.overview} ` ;
+  document.getElementById("title").innerHTML = warn;
+  document.getElementById("overview").innerHTML = warn2;
+  event.preventDefault();
+}
+
+function openNav() {
+    document.getElementById("boton").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("boton").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+*/
